663f33ee133bce4f9c74eb39de249069ec3a279f
Abraham9804 <romero9804@hotmail.com>
2026-02-27 19:29:10 -0800

    feat: create api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('customer', CustomerController::class);
+    Route::apiResource('invoice', InvoiceController::class);
+});
